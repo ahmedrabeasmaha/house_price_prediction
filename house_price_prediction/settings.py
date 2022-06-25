6be620dadf9c1e2c1769e53da566f1163fd09ab1
Ahmed Smaha <ahmed.rabea.smaha1999@gmail.com>
2022-06-25 04:34:33 +0200

    Initial Django App4

--- house_price_prediction/settings.py
+++ house_price_prediction/settings.py
@@ -94,8 +94,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
